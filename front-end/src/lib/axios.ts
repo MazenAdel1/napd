de086516ab002b26c533541cf3215e2a58fef995
MazenAdel1 <mazenadel.dev@gmail.com>
2025-11-21 20:12:09 +0200

    update last commit

--- front-end/src/lib/axios.ts
+++ front-end/src/lib/axios.ts
@@ -1,7 +1,8 @@
 import axs from "axios";
 
 const axios = axs.create({
-  baseURL: "https://abo-greda-production.up.railway.app/api",
+  // baseURL: "https://abo-greda-production.up.railway.app/api",
+  baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
 
