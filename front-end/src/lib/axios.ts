ee3c5dee6fd9cb882d3e194ef48569d6552de2f0
MazenAdel1 <mazenadel.dev@gmail.com>
2025-11-29 22:50:47 +0200

    update baseURL for axios

--- front-end/src/lib/axios.ts
+++ front-end/src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axs from "axios";
 
 const axios = axs.create({
-  // baseURL: "https://abo-greda-production.up.railway.app/api",
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://abo-greda-production.up.railway.app/api",
+  // baseURL: "http://localhost:3000/api",
   withCredentials: true,
 });
 
