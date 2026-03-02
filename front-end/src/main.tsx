755a25084aba94a884cfdf74c988330325b6eb07
MazenAdel1 <mazenadel.dev@gmail.com>
2026-03-02 23:36:17 +0200

    user Direction Shadcn component

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { DirectionProvider } from "./components/ui/direction.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <DirectionProvider dir="rtl">
+        <App />
+      </DirectionProvider>
     </BrowserRouter>
-  </StrictMode>
+  </StrictMode>,
 );
