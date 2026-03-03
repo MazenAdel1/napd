a78cb4dfe9c5dccf6aa263e1ed86a227c79b4f2d
MazenAdel1 <mazenadel.dev@gmail.com>
2026-03-03 21:05:28 +0200

    modify exports

--- front-end/src/components/admin/index.ts
+++ front-end/src/components/admin/index.ts
@@ -4,6 +4,5 @@ export { AdminTimeSlotsPage } from "./timeSlots";
 export { AdminAppointments } from "./appointments";
 export { Clients } from "./clients";
 export { AdminLayout } from "./layout";
-export { default as Reports } from "./reports/Reports";
-export { default as ReportCard } from "./reports/ReportCard";
-export { default as ReportLayout } from "./reports/ReportLayout";
+export { Reports, ReportCard, ReportLayout } from "./reports";
+export { Notifications } from "./notifications";
