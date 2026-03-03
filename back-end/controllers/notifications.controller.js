d21c9ed639039d53d9292ddd3cb91d49fafeaeec
MazenAdel1 <mazenadel.dev@gmail.com>
2026-03-03 21:24:29 +0200

    add notifications controller

---
+++ back-end/controllers/notifications.controller.js
@@ -0,0 +1,89 @@
+const asyncWrapper = require("../middleware/asyncWrapper");
+
+const { PrismaClient } = require("@prisma/client");
+const { withAccelerate } = require("@prisma/extension-accelerate");
+const { httpStatus } = require("../utils/consts");
+
+const prisma = new PrismaClient().$extends(withAccelerate());
+
+const getAllNotifications = asyncWrapper(async (req, res) => {
+  const { page = 1 } = req.query;
+  const limit = 10,
+    offset = (page - 1) * limit;
+
+  const [notifications, count] = await prisma.$transaction([
+    prisma.notification.findMany({
+      include: {
+        user: {
+          omit: { password: true },
+        },
+      },
+      skip: +offset,
+      take: +limit,
+      orderBy: { createdAt: "desc" },
+    }),
+    prisma.notification.count(),
+  ]);
+
+  const notReadCount = await prisma.notification.count({
+    where: { isRead: false },
+  });
+
+  return res.json({
+    status: httpStatus.SUCCESS,
+    data: {
+      notifications,
+      notReadCount,
+      totalPages: Math.ceil(count / limit),
+    },
+  });
+});
+
+const markNotificationsAsRead = asyncWrapper(async (req, res) => {
+  const { page = 1 } = req.query;
+  const limit = 10,
+    offset = (page - 1) * limit;
+
+  await prisma.notification.updateMany({
+    where: { isRead: false },
+    data: { isRead: true },
+  });
+
+  const notifications = await prisma.notification.findMany({
+    include: {
+      user: {
+        omit: { password: true },
+      },
+    },
+    skip: +offset,
+    take: +limit,
+    orderBy: { createdAt: "desc" },
+  });
+
+  return res.json({
+    status: httpStatus.SUCCESS,
+    data: { notifications, notReadCount: 0 },
+  });
+});
+
+const CreateNotification = async (userId, type) => {
+  const notification = await prisma.notification.create({
+    data: {
+      userId,
+      type,
+    },
+    include: {
+      user: {
+        omit: { password: true },
+      },
+    },
+  });
+
+  return notification;
+};
+
+module.exports = {
+  CreateNotification,
+  getAllNotifications,
+  markNotificationsAsRead,
+};
