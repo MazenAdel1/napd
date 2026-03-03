be9193223cbef094a52831da3c1ad44bf36835d6
MazenAdel1 <mazenadel.dev@gmail.com>
2026-03-03 21:22:09 +0200

    add new migrations

---
+++ back-end/prisma/migrations/20260302050440_add_notifications/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "public"."NotificationType" AS ENUM ('CLIENT_REGISTERED', 'APPOINTMENT_BOOKED', 'APPOINTMENT_CANCELLED');
+
+-- CreateTable
+CREATE TABLE "public"."Notification" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "type" "public"."NotificationType" NOT NULL,
+    "isRead" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."Notification" ADD CONSTRAINT "Notification_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
